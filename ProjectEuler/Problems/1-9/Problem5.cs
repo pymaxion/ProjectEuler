080c6dc0c87611591f740f3c6a3950981a0eda7f
Patrick Yukman <pymaxion@gmail.com>
2017-05-18 21:58:11 -0500

    went back to re-solve Problem 5 (previously solved prior to github project)

---
+++ ProjectEuler/Problems/1-9/Problem5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectEuler.Utils;
+
+namespace ProjectEuler.Problems
+{
+    public static class Problem5
+    {
+        public static string Solve()
+        {
+            int[] divisors = { 3, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+
+            long num = 20;
+            while (true)
+            {
+                bool evenlyDivided = true;
+                foreach (int divisor in divisors)
+                {
+                    if (num % divisor != 0)
+                    {
+                        evenlyDivided = false;
+                        break;
+                    }
+                }
+
+                if (evenlyDivided)
+                {
+                    return num.ToString();
+                }
+
+                num += 20;
+            }
+        }
+    }
+}
